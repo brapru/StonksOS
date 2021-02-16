ecfc4bc356d086b7c5d76f7b42cbd8a3bf846fbb
0xada4d <adam@0xadam.com>
2021-02-15 20:48:36 -0500

    some refactoring

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -8,12 +8,10 @@ extern "C" [[noreturn]] void kernel_main(){
 	Stdio stdio;
 	MiniUart mu;
 
-	stdio.printf("\r\n\r\n\r\nWelcome to StonksOS.");
-	stdio.puts("\r\n\r\n\r\nWelcome to StonksOS.\r\n");
-
+	stdio.puts("\n\nWelcome to StonksOS.\n");
 	stdio.puts("Working at exception level: ");
-
 	mu.uart_hex(kernel::get_el());
+	stdio.puts("\n");
 
 	while(1){
 		char c = mu.uart_recv();
