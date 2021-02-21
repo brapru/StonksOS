13df5f524192c783fc992ba9b5e20a62a18654ef
brapru <smith.pruitt@gmail.com>
2021-02-21 18:32:00 -0500

    updated kernel.cpp to match main

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -6,23 +6,24 @@
 /*  StonksOS kernel entry point */
 extern "C" [[noreturn]] void kernel_main(){
 	
-	Stdio stdio;
+	//Stdio stdio;
 	MiniUart mu;
+	Stdio::init_miniuart(&mu);
 
 	irq_init_vectors();
 	IRQ::enable_interrupt_controller();
 	irq_enable();
 	
-	stdio.printf("\nValue of mu lcr is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_lcr);
-	stdio.printf("\nValue of mu mcr is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_mcr);
-	stdio.printf("\nValue of mu ier is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_ier);
-	stdio.printf("\nValue of mu iir is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_iir);
-	stdio.printf("\nValue of mu enables is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->enables);
-	stdio.printf("\nValue of mu control is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_control);
-	stdio.printf("\nValue of mu baud is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_baud_rate);
+	Stdio::printf("\nValue of mu lcr is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_lcr);
+	Stdio::printf("\nValue of mu mcr is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_mcr);
+	Stdio::printf("\nValue of mu ier is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_ier);
+	Stdio::printf("\nValue of mu iir is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_iir);
+	Stdio::printf("\nValue of mu enables is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->enables);
+	Stdio::printf("\nValue of mu control is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_control);
+	Stdio::printf("\nValue of mu baud is: %x\n", mu.get_aux_object()->get_aux_regs_ptr()->mu_baud_rate);
 	
-	stdio.puts("\n\nWelcome to StonksOS.\n");
-	stdio.printf("Working at exception level: %d\n", kernel::get_el());
+	Stdio::puts("\n\nWelcome to StonksOS.\n");
+	Stdio::printf("Working at exception level: %d\n", kernel::get_el());
 
 	while(1){
 		//char c = mu.uart_recv();
